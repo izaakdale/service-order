020951d971493d84de1c78f16deb58417f2d3823
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-10-11 21:29:45 +0100

    Added env variable to listen on

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 PROTO_DIR=pkg/model/order
 
+run:
+	SERVER_ADDRESS="8080" go run main.go
+
 gproto:
 	protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ${PROTO_DIR}/*.proto \
 	 --go-grpc_out=. --go-grpc_opt=paths=source_relative ${PROTO_DIR}/*.proto
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/izaakdale/service-order/service"
 )
 
 func main() {
 	service := service.New()
-	log.Fatal(http.ListenAndServe("localhost:8080", service.Router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("SERVER_ADDRESS")), service.Router))
 }
